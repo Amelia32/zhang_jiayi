422c3447c57fd0bd805d2287ef6f3f1b746bae45
zhangjiayi <784001396@qq.com>
2018-10-27 19:53:33 +0800

    第四单元作业（最新改正版）

--- chap02/ex_271/main.cpp
+++ chap02/ex_271/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-
-int main(int argc, char **argv)
-{
-    std::cout << "Hello World" << std::endl;
-    return 0;
-}
+  #include <math.h>
+  using namespace std;
+  int main()
+  {
+      int b=0, sum=0;
+      int a;
+      cout<<"Please enter a binary number";
+      cin>>a;
+      int t;
+      int d;
+      do
+      {
+          b++;
+          t=a%10;
+          a/=10;
+          d=pow(2,b);
+          sum=sum+t*d;
+      }
+      while((a=0));
+      cout<<"The decimal system is:"<<sum<<endl;
+  } 
+  
