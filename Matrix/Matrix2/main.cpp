16a8aeda064fb9984f991b22a916eea52959b83a
zhangjiayi <784001396@qq.com>
2019-03-24 18:46:49 +0800

    第四次提交作业

---
+++ Matrix/Matrix2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int line, row;
+    Matrix A1, A2, A3;
+    Matrix* pA1 = new Matrix;
+    Matrix* pA2 = new Matrix;
+    Matrix* pA3 = new Matrix;
+}
